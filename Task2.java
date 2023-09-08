11324af02461f12af66092b56d707494d562b584
Oleg93QA <mironov1993@bk.ru>
2023-09-08 10:58:04 +0400

    задание2

---
+++ Task2.java
@@ -0,0 +1,11 @@
+package IBSjavaBasic;
+
+public class Task2 {
+    public static void main(String[] args) {
+        double a;
+        byte b = -128;
+        a = 4.9 * 10 * 10308;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
